fe8f8cc285b183923e1fe1dbb678ad6448749ea8
Whitman Johnson <whit.ross.j@gmail.com>
2018-03-15 16:40:23 -0400

    fix(App): android arial font fix

--- native-client/App.js
+++ native-client/App.js
@@ -1,44 +1,65 @@
 import React from 'react'
-import { Alert, Button, TouchableHighlight, StyleSheet, Text, View } from 'react-native'
+import { Alert, Button, TouchableHighlight, StyleSheet, Text, View, Platform } from 'react-native'
+import { Font, AppLoading } from 'expo'
 
 let API_ROOT
 // if (IS_SIM) {
 //   API_ROOT = 'http://localhost:5000'
 // } else {
-  API_ROOT = 'http://192.168.1.190:5000'
+API_ROOT = 'http://192.168.1.190:5000'
 // }
 
 export default class App extends React.Component {
-  _onPressButton() {
-    fetch(`${API_ROOT}/users`)
-    .then((res) => res.text())
-    .then((data) => Alert.alert(data))
-    // Alert.alert('You tapped the button!');
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>fetch</Text>
-        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
-          <View>
-            <Text>get started</Text>
-          </View>
-        </TouchableHighlight>
-      </View>
-    )
-  }
+    constructor(props){
+        super(props)
+        this.state = {
+            fontLoaded: false
+        }
+    }
+
+    componentWillMount() {
+        if (Platform.OS === 'android'){
+            Font.loadAsync({
+                Arial: require('./fonts/Arial.ttf')
+            })
+                .then(() => this.setState({ fontLoaded: true }))
+        }
+        else this.setState({ fontLoaded: true })
+    }
+
+    _onPressButton() {
+        fetch(`${API_ROOT}/users`)
+            .then((res) => res.text())
+            .then((data) => Alert.alert(data))
+        // Alert.alert('You tapped the button!');
+    }
+    render() {
+        if (!this.state.fontLoaded){
+            return <AppLoading />
+        }
+        return (
+            <View style={styles.container}>
+              <Text style={styles.title}>fetch</Text>
+              <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+                <View>
+                  <Text>get started</Text>
+                </View>
+              </TouchableHighlight>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e0ecef',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontFamily: 'Arial',
-    fontSize: 70,
-    color: '#b77575'
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#e0ecef',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontFamily: 'Arial',
+        fontSize: 70,
+        color: '#b77575'
+    }
 })
