0ac808b3df5be6cfaf49d43963f2b75819650d8c
Whitman Johnson <whit.ross.j@gmail.com>
2018-03-19 12:16:38 -0400

    feature: primitive login retrieval

--- native-client/src/components/LoginOrNavigate.js
+++ native-client/src/components/LoginOrNavigate.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import MainTabNavigator from '../navigation/MainTabNavigator.js'
-import { View, Text } from 'react-native'
+import { View, Text, AsyncStorage as store, Button } from 'react-native'
+
+const getLogin = async () => {
+    let login = await store.getItem('login')
+    login = JSON.parse(login)
+    return login
+}
 
 const Login = props => {
     return (
         <View>
           <Text>Hello </Text>
+          <Button
+            title="LogmeIn"
+            onPress={() => store.setItem('login', JSON.stringify(true))}
+            />
         </View>
     )
 }
 
-const LoginOrNavigate = props => {
-    const loggedin = false
-    return (loggedin) ? <MainTabNavigator /> : <Login />
+class LoginOrNavigate extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            loggedin: false
+        }
+    }
+
+    componentWillMount(){
+        getLogin()
+            .then(loggedin => {
+                this.setState({loggedin})
+            })
+    }
+
+    render(){
+        store.setItem('login', JSON.stringify(false))
+        const loggedin = this.state.loggedin
+        return (loggedin) ? <MainTabNavigator /> : <Login />
+    }
 }
 
 export default LoginOrNavigate
