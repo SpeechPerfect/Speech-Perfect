d16cc879a4bc1847befdc4a3e3c3ec07e44ca087
Sarah Grossman <sarah.beth.grossman@gmail.com>
2018-03-20 12:36:22 -0400

    feat: add seed file

--- native-client/src/screens/Profile.js
+++ native-client/src/screens/Profile.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react'
 import { Text, ScrollView, View, AsyncStorage as store, Button, TextInput, StyleSheet} from 'react-native'
 
 export default class Profile extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: null,
+      id: null
+    }
+  }
 
   static navigationOptions = {
     title: 'Profile',
   };
 
+  componentDidMount() {
+    store.getItem('user')
+    .then(userData => JSON.parse(userData))
+    .then((data) => {
+      console.log('data is', data)
+      this.setState({
+      email: data.email,
+      id: data.id
+    })
+    })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <ScrollView style={styles.container}>
-      <Text>Hello</Text>
+      <Text></Text>
       </ScrollView>
     )
   }
