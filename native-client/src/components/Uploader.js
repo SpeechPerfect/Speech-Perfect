2f3072f002d35826e5f75349bece6eac2f529931
Sarah Grossman <sarah.beth.grossman@gmail.com>
2018-03-22 16:10:32 -0400

    feat: send user data with audio

--- native-client/src/components/Uploader.js
+++ native-client/src/components/Uploader.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react'
-import { Button, View, AsyncStorage } from 'react-native'
+import { Button, View, AsyncStorage as store } from 'react-native'
 import API_ROOT from '../../IP_addresses'
 
 class Uploader extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      // user: AsyncStorage.getItem('');
+      userId: ''
     }
     this.onSubmit = this.onSubmit.bind(this)
   }
 
-  onSubmit() {
-    const data = new FormData()
-    data.append("soundFile", {
-      uri: this.props.uri,
-      type: "audio/vnd.wav",
-      name: "testAudio"
-    })
+  componentDidMount() {
+    store.getItem('user')
+    .then((user) => this.setState({
+      userId: user.id
+    }))
+  }
 
+  onSubmit() {
+    store.getItem('user')
+    .then(user => JSON.parse(user))
+    .then(userData =>
+      {
+      let data = new FormData()
+      data.append("soundFile", {
+        uri: this.props.uri,
+        type: "audio/vnd.wav",
+        name: "testAudio",
+        })
+      data.append('userId', userData.id)
+      return data
+    })
+    .then((returnedData) => {
+      console.log('RETURNED DATA IS ', returnedData)
+      fetch(`${API_ROOT}/api/audio/upload`, {
+        method: "post",
+        body: returnedData,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+    })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => console.log(err))
+  }
     //SEND TO AWS
     // fetch(`${API_ROOT}/api/audio/upload`, {
     //   method: "post",
@@ -33,19 +62,7 @@ class Uploader extends Component {
     //   })
     //   .catch(err => console.log(err))
 
-    fetch(`${API_ROOT}/api/watson-api`, {
-      method: "post",
-      body: data,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'multipart/form-data',
-      },
-    })
-      .then(res => {
-        console.log(res)
-      })
-      .catch(err => console.log(err))
-  }
+
 
   render() {
     return (
--- server/api/audio.js
+++ server/api/audio.js
@@ -1,19 +1,18 @@
 const router = require('express').Router()
-const { awsReport } = require('../db/models')
+const { AwsReport, WatsonReport, Speech, User } = require('../db/models')
 const multer  = require('multer')
 const fs = require('fs')
 const multerS3 = require('multer-s3')
 const AWS = require('aws-sdk')
 const creds = require('../secrets')
+const upload = multer({dest: '/tmp'})
 
-// AWS.config.loadFromPath('./s3_config.json')
+// AWS funcs
 
 AWS.config.update(creds.creds)
-
-
 const s3 = new AWS.S3()
 
-const upload = multer({
+const awsUpload = multer({
   storage: multerS3({
     s3: s3,
     bucket: creds.bucket,
@@ -22,15 +21,49 @@ const upload = multer({
       return cb(null, 'audio/x-wav')
     }, // multerS3.AUTO_CONTENT_TYPE,
     metadata: function (req, file, cb) {
+      console.log('FILE IS ', file)
       cb(null, {fieldName: file.fieldname})
     },
     key: function (req, file, cb) {
+
       cb(null, Date.now().toString())
     }
   })
 })
 
-router.post('/upload', upload.single('soundFile'), (req, res, next) => {
+// Watson funcs
+
+const { promisify } = require('util')
+var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1')
+
+let dataAnalysis = async (params) => {
+  var speechToText = new SpeechToTextV1({
+    username: process.env.WATSON_USERNAME,
+    password: process.env.WATSON_PASSWORD,
+    url: 'https://stream.watsonplatform.net/speech-to-text/api/'
+  })
+
+ const speechToTextPromisified = promisify(speechToText.recognize.bind(speechToText))
+ const { results } = await speechToTextPromisified(params)
+ return results
+}
+
+// add regex to account for end of sentence
+
+let analyzeTranscript = (str) => {
+  let obj = {}
+  obj.umCount = 0
+  obj.likeCount = 0
+  let strArr = str.split(' ')
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] === 'um' || strArr[i] === '%HESITATION') obj.umCount++
+    if (strArr[i] === 'like') obj.likeCount++
+  }
+  return obj
+}
+
+router.post('/upload', awsUpload.single('soundFile'), (req, res, next) => {
+  console.log(req.body, 'is req.body!!!!')
   console.log('in post route, file is ', req.file)
   res.send('Successfully uploaded ' + req.file.fieldName + ' file!')
 })
