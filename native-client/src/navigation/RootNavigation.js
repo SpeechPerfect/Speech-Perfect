e72c02948265bcb648b4e39564bd9e64fbe8a489
Sarah Grossman <sarah.beth.grossman@gmail.com>
2018-03-25 18:28:33 -0400

    feat: working signup

--- native-client/src/navigation/RootNavigation.js
+++ native-client/src/navigation/RootNavigation.js
@@ -1,64 +1,7 @@
-// import { Notifications } from 'expo'
 import React from 'react'
 import { StackNavigator } from 'react-navigation'
 import {SignedInNav} from './MainTabNavigator'
 import {SignedOutNav} from './SignedOutNavigator'
-// import {SingleReport, WordRepetition, AuthScreen, LoginScreen, SignupScreen} from '../screens'
-// import {  View, StyleSheet, AsyncStorage as store } from 'react-native'
-
-
-// const RootStackNavigator = StackNavigator(
-//   {
-//     Main: {
-//       screen: SignedInNav
-//     },
-//     // SingleReport: {
-//     //   screen: SingleReport,
-//     // },
-//     // WordRepetition: {
-//     //   screen: WordRepetition
-//     // },
-//     // AuthScreen: {
-//     //   screen: AuthScreen
-//     // },
-//     // Login: {
-//     //   screen: LoginScreen
-//     // },
-//     // Signup: {
-//     //   screen: SignupScreen
-//     // },
-//     initialRouteName: 'Main'
-//   },
-//   // {
-//   //   navigationOptions: () => ({
-//   //     headerTitleStyle: {
-//   //       fontWeight: 'normal',
-//   //     },
-//   //   }),
-//   // }
-// )
-
-// export default class RootNavigator extends React.Component {
-
-//   render() {
-//     return (
-//     <View style={styles.container}>
-//         <SignedInNav />
-//     </View>
-//     )
-//   }
-
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   smallContainer: {
-//     flexGrow: 10,
-//     backgroundColor: 'black',
-//   },
-// })
 
 const createRootNavigator = (signedIn = false) => {
   return StackNavigator(
