2ac782aaa8322e859373756917d7baf8cab0ea69
Whitman Johnson <whit.ross.j@gmail.com>
2018-03-26 18:21:20 -0400

    chore(RecorderRender): formatting

--- native-client/src/components/RecorderRender.js
+++ native-client/src/components/RecorderRender.js
@@ -17,7 +17,7 @@ const RecorderRender = props => {
     const { onButtonClear, startRecording, stopRecording } = props.buttons
     let buttonMethod
     buttonMethod = state.isRecording ? stopRecording : startRecording
-    return  (
+    return (
         <View style={styles.container}>
           <View style={styles.recorderHeader}>
             <RenderHeader />
@@ -27,42 +27,51 @@ const RecorderRender = props => {
           </View>
           <View style={styles.recorderBottomContainer}>
             <RecordButton press={buttonMethod} />
-            {state.begin ?
-                <View style={{height: 2, marginBottom: -2}}>
-                      <Text style={{color: 'white'}}>''</Text>
+            {
+                state.begin ? (
+                    <View style={{height: 2, marginBottom: -2}}>
+                      <Text style={{color: 'white'}}>
+                        ''
+                      </Text>
                     </View>
-                    :
+                ) : (
                     <View style={styles.recorderBottomText}>
-                          <Text style={styles.recorderIntroText} > Press the mic and start speaking </Text>
-                              <Text style={styles.recorderText}> We will analyze your speech and </Text>
-                                  <Text style={styles.recorderText}> provide you with suggestions on how to improve </Text>
-                        </View>
-                    }
-                    {(state.begin && state.isClicked) ?
+                      <Text style={styles.recorderIntroText} > Press the mic and start speaking </Text>
+                      <Text style={styles.recorderText}>
+                        We will analyze your speech and
+                      </Text>
+                      <Text style={styles.recorderText}> provide you with suggestions on how to improve </Text>
+                    </View>
+                )
+            } {
+                (state.begin && state.isClicked)
+                    ? (
                         <View style={styles.recorderIntroText}>
-                              <Text style={styles.recorderStopText} > Press the mic to stop recording </Text>
-                            </View>
-                            :
-                            <View style={styles.recorderBottomText}>
-                                  <Text style={styles.recorderStopText} />
-                                </View>
-                            }
-                            <View style={styles.recorderBottomText}>
-                              {state.begin &&
-                                  <View style={styles.recorderButtons}>
-                                        <View style={styles.recorderUpload}>
-                                              <Button style={styles.recorderButton} color="white" onPress={onButtonClear} title="reset" />
-                                            </View>
-                                            {!state.isClicked &&
-                                             <View style={styles.recorderUpload}>
-                                                   <Uploader navigation={props.navigation} uri={state.recording._uri} duration={state.durationMillis} />
-                                                 </View>
-                                                 }
-                                      </View>
-                                  }
-                            </View>
-          </View>
-        </View>
+                          <Text style={styles.recorderStopText} > Press the mic to stop recording </Text>
+                        </View>
+                    ) : (
+                        <View style={styles.recorderBottomText}>
+                          <Text style={styles.recorderStopText} />
+                        </View>
+                    )
+            }
+            <View style={styles.recorderBottomText}>
+            {
+                state.begin && (
+                    <View style={styles.recorderButtons}>
+                      <View style={styles.recorderUpload}>
+                        <Button style={styles.recorderButton} color="white" onPress={onButtonClear} title="reset" />
+                      </View>
+                      {!state.isClicked && (
+                          <View style={styles.recorderUpload}>
+                            <Uploader navigation={props.navigation} uri={state.recording._uri} duration={state.durationMillis} />
+                          </View>
+                      )}
+                    </View>
+                )
+            } </View>
+            </View>
+            </View>
     )
 }
 
