f29a4ef36277c6f33f59c69df44f75ae9c1aeaf3
Sarah Grossman <sarah.beth.grossman@gmail.com>
2018-03-28 15:41:12 -0400

    refactor: added config file

---
+++ server/config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+{
+"production":
+  {
+  "username": null,
+  "password": null,
+  "database": "DATABASE_URL",
+  "dialect": "postgres"
+  "use_env_variable": "DATABASE_URL"
+  }
+}
+/* eslint-enable */
