69f77fc00dcf213f9d4829657076586e98d02df3
danmcgill6 <danmcgill6@gmail.com>
2018-03-28 15:36:35 -0400

    style(SpeechList): prettier

--- native-client/src/components/SpeechList.js
+++ native-client/src/components/SpeechList.js
@@ -1,75 +1,79 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { Text, View } from 'react-native'
 import styles from '../../assets/stylesheet'
 import axios from 'axios'
 import API_ROOT from '../../IP_addresses.js'
-import { LinearGradient } from 'expo';
-
+import { LinearGradient } from 'expo'
 
 export default class SpeechList extends Component {
   static navigationOptions = {
     title: 'SingleReport'
-  };
+  }
 
   constructor(props) {
     super(props)
     this.state = {
-        speechData: {}
+      speechData: {}
     }
   }
 
   componentDidMount = () => {
     console.log('SPEECH ID IS ', this.props.speechId)
-    axios.get(`${API_ROOT}/api/speech/watson-data/${this.props.speechId}`)
-    .then((res) => res.data)
-    .then((speechData) => {
-      console.log('speech data is', speechData)
-      this.setState({
-        speechData
+    axios
+      .get(`${API_ROOT}/api/speech/watson-data/${this.props.speechId}`)
+      .then(res => res.data)
+      .then(speechData => {
+        console.log('speech data is', speechData)
+        this.setState({
+          speechData
+        })
       })
-    })
   }
 
-
-  renderItems(speechData){
+  renderItems(speechData) {
     return speechData.map(data => {
       let label = data[0]
       let value = data[1]
-      return(
+      return (
         <View style={styles.speechListItem}>
-        <LinearGradient colors={['#5e4ba0','#413372']} style={styles.linearGradientPurple}/>
+          <LinearGradient
+            colors={['#5e4ba0', '#413372']}
+            style={styles.linearGradientPurple}
+          />
           <Text style={styles.speechItemLabel}>{label}</Text>
           <Text style={styles.speechItemValue}>{value}</Text>
         </View>
       )
     })
   }
-render() {
-  console.log('IN SPEECH LIST, state is ', this.state)
+  render() {
+    console.log('IN SPEECH LIST, state is ', this.state)
 
-  const { duration, likeCount, umCount, wordCount } = this.state.speechData
+    const { duration, likeCount, umCount, wordCount } = this.state.speechData
 
-  let pace = wordCount / (duration / 60000)
+    let pace = wordCount / (duration / 60000)
 
-  let minutes = Math.floor(duration / 60000) || 0
-  let seconds = Math.floor(duration / 1000) % 60 || 0
-  let  milliseconds = duration % 1000 || '00'
+    let minutes = Math.floor(duration / 60000) || 0
+    let seconds = Math.floor(duration / 1000) % 60 || 0
+    let milliseconds = duration % 1000 || '00'
 
-  minutes = minutes.toString().length === 1 ? `0${minutes}` : minutes
-  seconds = seconds.toString().length === 1 ? `0${seconds}` : seconds
-  milliseconds = milliseconds.toString().slice(0, 2)
+    minutes = minutes.toString().length === 1 ? `0${minutes}` : minutes
+    seconds = seconds.toString().length === 1 ? `0${seconds}` : seconds
+    milliseconds = milliseconds.toString().slice(0, 2)
 
-  let speechData = [['Duration: ', `${minutes}:${seconds}.${milliseconds}`], ['Word Count: ', wordCount], ['Pace: ', `${pace} wpm`], ['Um Count: ', umCount], ['Like Count: ', likeCount]]
+    let speechData = [
+      ['Duration: ', `${minutes}:${seconds}.${milliseconds}`],
+      ['Word Count: ', wordCount],
+      ['Pace: ', `${pace} wpm`],
+      ['Um Count: ', umCount],
+      ['Like Count: ', likeCount]
+    ]
 
-
-  return (
-    <View style={styles.speechListContainer}>
-    {
-        this.state.speechData && this.renderItems(speechData)
-    }
-  </View>
-  )
+    return (
+      <View style={styles.speechListContainer}>
+        {this.state.speechData && this.renderItems(speechData)}
+      </View>
+    )
+  }
 }
-}
-
