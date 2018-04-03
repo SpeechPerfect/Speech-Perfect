f937c723c878ce03e790ff21c727d49b5e13c1d4
Whitman Johnson <whit.ross.j@gmail.com>
2018-04-03 16:09:18 -0400

    refactor(LoginScreen.test): login testing no longer utilizes timeout

--- native-client/src/screens/LoginScreen.test.js
+++ native-client/src/screens/LoginScreen.test.js
@@ -3,20 +3,18 @@ import Adapter from 'enzyme-adapter-react-16'
 import Enzyme from 'enzyme'
 Enzyme.configure({ adapter: new Adapter() })
 
-import Login from './Login'
+import LoginScreen from './LoginScreen'
 
-it('Can insert a user', async () => {
-    function sleep(ms) { //https://stackoverflow.com/a/39914235
-        return new Promise(resolve => setTimeout(resolve, ms))
-    }
-    expect.assertions(1)
-    let email
-    const rendered = Enzyme.shallow(<Login />)
-    rendered.setState( {email: 'Hello@hello.hello', password: '123'} )
-    let button = rendered.find('.login')
-    button.simulate('press')
-    //wait to give the button time to work
-    await sleep(2000)
-    email = rendered.state().user.email
-    expect(email).toBe('Hello@hello.hello')
+it('Can insert a user', () => {
+  expect.assertions(1)
+  const rendered = Enzyme.shallow(<LoginScreen />)
+  rendered.setState({ email: 'Hello@hello.hello', password: '123' })
+  console.log('LOUDNESS', rendered.instance())
+  let email = rendered
+    .instance()
+    .onButtonPress()
+    .then(() => {
+      return rendered.instance().state.loggedin
+    })
+  return expect(email).resolves.toBe(true)
 })
