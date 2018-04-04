be2f636826c8d83f7a5a12a7b77170df5f67beab
Whitman Johnson <whit.ross.j@gmail.com>
2018-04-04 11:56:23 -0400

    test: added signup testing

---
+++ native-client/src/screens/SignupScreen.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme from 'enzyme'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import API_ROOT from '../../IP_addresses'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+import SignupScreen from './SignupScreen'
+
+describe('Signup', () => {
+  let mockAxios = new MockAdapter(axios)
+  const fakeData = JSON.stringify('fakeUserData')
+  it('requests on the correct route', () => {
+    mockAxios.onPost(`${API_ROOT}/auth/signup`).replyOnce(200, fakeData)
+    expect.assertions(1)
+    const rendered = Enzyme.shallow(<SignupScreen />)
+    rendered.setState({
+      email: 'new@email.mail',
+      password: '123',
+      confirmPassword: '123'
+    })
+    console.log('LOUDNESS', rendered.instance())
+    let loggedin = rendered
+      .instance()
+      .onButtonPress()
+      .then(() => {
+        return rendered.instance().state.loggedin
+      })
+    return expect(loggedin).resolves.toBe(true)
+  })
+})
